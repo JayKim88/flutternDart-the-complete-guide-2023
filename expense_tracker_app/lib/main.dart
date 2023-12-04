fbdeb3110f55263de24bee10eebd96df1d85651a
Yongjae Kim <mearak43@gmail.com>
2023-12-05 08:39:23 +0900

    progressed from 120-122, section5

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -5,7 +5,10 @@ import 'package:expense_tracker_app/widgets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 255, 180, 175),
+      ),
       home: const Expenses(),
     ),
   );
